4dcb0794df4ff6f64e7f33035fc3e0a79219727d
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2022-01-20 10:10:30 -0300

    Improving code with iterator features.

--- src/lib.rs
+++ src/lib.rs
@@ -34,17 +34,15 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 2 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = if args.len() < 3 {
-            None
-        } else {
-            Some(args[2].clone())
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
         };
+
+        let filename = args.next();
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
@@ -57,15 +55,10 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
